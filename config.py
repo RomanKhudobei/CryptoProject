bb6ee7ddd5ec3e4f86c11d6e0723ef333c9a1276
Roman <roman.khudobei@gmail.com>
2018-02-13 11:12:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,15 @@
 HOST = ''
 USER = ''
 PASSWORD = ''
+PORT = ''
 
 DATABASE_NAME = 'transactions'
 MAIN_TABLE_NAME = 'transactions'
 LOG_TABLE_NAME = 'log'
 
-API_KEY = ''
+API_KEY = '2UPGUXGVPWH9WTCSMB387Z6W1JSQSANPRD'
 
-ADDRESS = ''
+ADDRESS = '0xf872e98dbe7e7f2a6abfa35e5c8c2bc9fe4e2ca7'
 
 # Time to check oldness of transaction
 # How old transactions you want to add to your table
@@ -17,11 +18,15 @@ ADDRESS = ''
 # if transaction older than TIME_TO_CHECK, then script will stop iterating
 # *NOTE: order of transactions is descending (from new to old)
 TIME_TO_CHECK = {
-	'days': 1,
-	'seconds': 0,
-	'microseconds': 0,
-	'milliseconds': 0,
-	'minutes': 0,
-	'hours': 0,
-	'weeks': 0
+    'days': 1,
+    'seconds': 0,
+    'microseconds': 0,
+    'milliseconds': 0,
+    'minutes': 0,
+    'hours': 0,
+    'weeks': 0
 }
+
+APP_SECRET_KEY = 'super_secret_key'		# For session creation
+APP_HOST = '0.0.0.0'	# localhost
+APP_PORT = 5000
